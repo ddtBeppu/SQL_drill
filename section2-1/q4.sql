a9c06ee884e4b797c8cf28ad78c6ddc5e90e9dbe
Naoto Beppu <ddt.naoto@gmail.com>
2018-09-20 20:54:42 +0900

    2-1 問4 ユーザIDでグループ化し、ユーザをそれぞれ一意としてソートしました。

--- section2-1/q4.sql
+++ section2-1/q4.sql
@@ -5,32 +5,33 @@
 */
 
 /** 解答 **/
-mysql> SELECT -- 選択
-    ->     CONCAT(usr.l_name, usr.f_name) AS 氏名, -- 氏名
-    ->     COUNT(rent.id) AS 貸し出し数 -- 貸し出し数
-    -> FROM -- テーブルを指定
-    ->     rental AS rent -- 貸し出しテーブル
-    -> INNER JOIN -- テーブルを結合
-    ->     usr -- ユーザテーブル
-    -> ON -- 以下のカラム同士でテーブルを結合
-    ->     rent.user_id = usr.user_id -- ユーザID同士でテーブルを結合
-    -> WHERE -- 以下の条件で検索
-    ->     rent.returned = 0 -- 貸出中の場合
-    -> GROUP BY -- 以下の項目でまとめる
-    ->     氏名 -- 氏名ごとにデータをまとめる
-    -> ORDER BY -- 以下の項目についてデータをソート
-    ->     COUNT(rent.id) DESC -- 貸し出し数について降順
-    -> ;
+SELECT -- 選択
+    CONCAT(usr.l_name, usr.f_name) AS 氏名, -- 氏名
+    COUNT(rent.id) AS 貸し出し数 -- 貸し出し数
+FROM -- テーブルを指定
+    rental AS rent -- 貸し出しテーブル
+INNER JOIN -- テーブルを結合
+    usr -- ユーザテーブル
+ON -- 以下のカラム同士でテーブルを結合
+    rent.user_id = usr.user_id -- ユーザID同士でテーブルを結合
+WHERE -- 以下の条件で検索
+    rent.returned = 0 -- 貸出中の場合
+GROUP BY -- 以下の項目でまとめる
+    usr.user_id -- ユーザごとにデータをまとめる
+ORDER BY -- 以下の項目についてデータをソート
+    COUNT(rent.id) DESC -- 貸し出し数について降順
+;
+
 +--------------+-----------------+
 | 氏名         | 貸し出し数      |
 +--------------+-----------------+
 | 西島文子     |               2 |
 | 河合泰治     |               2 |
+| 松本博美     |               1 |
 | 上原幸一     |               1 |
 | 和田修子     |               1 |
 | 森本絵里     |               1 |
 | 根岸裕子     |               1 |
 | 吉岡遥       |               1 |
-| 松本博美     |               1 |
 +--------------+-----------------+
 8 rows in set (0.00 sec)
