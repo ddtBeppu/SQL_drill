a533c271ea54a23c2a953a301cc437d4481ce5fd
naoto <ddt.beppu@gmail.com>
2018-10-23 19:47:58 +0900

    3-7 問3 解答しました。

--- section3-7/q3.sql
+++ section3-7/q3.sql
@@ -4,3 +4,13 @@
 */
 
 /** 解答 **/
+DELETE  FROM -- テーブルからデータを削除
+    employee -- 社員テーブル
+WHERE -- 条件で絞りこみ
+        retired = 1 -- 退職済み
+    AND -- かつ
+        last_update < '2007-03-31' -- 最終更新日が2007-03-31以前
+;
+
+
+Query OK, 1 row affected (0.03 sec)
